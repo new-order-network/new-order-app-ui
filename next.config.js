6b9f9f9ba488e8cb2dc31c3ea92230a711a9224f
Arjan Heaven Villon <heaven.arjan@gmail.com>
2022-10-31 22:11:32 +0800

    fix: next config warnings

--- next.config.js
+++ next.config.js
@@ -1,11 +1,7 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 /** @type {import('next').NextConfig} */
 
-const withPlugins = require('next-compose-plugins')
 const withSvgr = require('next-svgr')
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: process.env.ANALYZE === 'true',
-})
 
 const nextConfig = {
   reactStrictMode: true,
@@ -20,4 +16,4 @@ const nextConfig = {
   },
 }
 
-module.exports = withPlugins([withBundleAnalyzer, withSvgr], nextConfig)
+module.exports = withSvgr(nextConfig)
